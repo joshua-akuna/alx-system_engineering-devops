7cca3769dbbde40be15314ee963f942f21de143b
Akuna Joshua <akunajoshua@gmail.com>
2023-09-22 18:13:30 +0100

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in /tmp with specified permissions, owner, group and content.
+file {'/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
