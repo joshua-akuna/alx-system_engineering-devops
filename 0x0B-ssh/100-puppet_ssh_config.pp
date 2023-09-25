c333eb9f90a9c6b4b550841727a8d6f13e70d70f
Akuna Joshua <akunajoshua@gmail.com>
2023-09-25 08:32:50 +0100

    100-puppet_ssh_config.pp : correction : syntax error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Editing the client config file
 include stdlib
 
-file_line { 'refuse passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'use private key':
+file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '     IdentityFile ~/.ssh/school',
