03d6d022c85f5bf9a8d74e431cc3a6cf69fec517
Akuna Joshua <akunajoshua@gmail.com>
2023-10-02 18:38:44 +0100

    2-puppet_custom_http_response_header.pp : correction : output error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 # This manifest installs and configures nginx using puppet
 
 exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx',
   provider => shell,
 }
 
 exec {'add custom header':
-  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\n\tadd_header X-Served-By $(hostname);/" /etc/nginx/nginx.conf',
+  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\n\tadd_header X-Served-By \"$(hostname)\";/" /etc/nginx/nginx.conf',
   provider => shell,
 }
 
