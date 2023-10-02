88aee3333c931082fb97a8d21b4dcd0bd6c9ea04
Akuna Joshua <akunajoshua@gmail.com>
2023-10-02 21:28:33 +0100

    2-puppet_custom_http_response_header.pp : correction : output error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,16 +3,19 @@
 exec {'update':
   command  => 'sudo apt-get -y update',
   provider => shell,
+  before   => Exec['install'], 
 }
 
 exec {'install':
   command  => 'sudo apt-get -y install nginx',
   provider => shell,
+  before   => Exec['add custom header'],
 }
 
 exec {'add custom header':
   command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\n\tadd_header X-Served-By \"$(hostname)\";/" /etc/nginx/nginx.conf',
   provider => shell,
+  before   => Exec['restart nginx'],
 }
 
 exec {'restart nginx':
