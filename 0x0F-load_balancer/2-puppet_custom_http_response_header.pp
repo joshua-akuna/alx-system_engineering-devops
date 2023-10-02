625f48a2da8f70149935cae69aca72ee583690af
Akuna Joshua <akunajoshua@gmail.com>
2023-10-02 17:20:10 +0100

    2-puppet_custom_http_response_header.pp file commit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# This manifest installs and configures nginx using puppet
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec {'index page':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'permanent redirect':
+  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default',
+  provider => shell,
+}
+
+exec {'add custom header':
+  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\n\tadd_header X-Served-By $HOSTNAME;/" /etc/nginx/nginx.conf',
+  provider => shell,
+}
+
+exec {'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
