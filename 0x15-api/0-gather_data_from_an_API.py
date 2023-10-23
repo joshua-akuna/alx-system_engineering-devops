f3a999aa92f9a529cb5366a53f70d3ec5153bb1c
Akuna Joshua <akunajoshua@gmail.com>
2023-10-23 17:24:31 +0100

    0-gather_data_from_an_API.py : correction : pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,14 +9,15 @@ import sys
 
 
 if __name__ == '__main__':
-    if (len(sys.argv) == 2):
+    if len(sys.argv) == 2:
         user_id = int(sys.argv[1])
         all_tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
-        user = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+        user = requests.get(
+                f'https://jsonplaceholder.typicode.com/users/{user_id}')
         total_tasks_by_user_id = [task for task in all_tasks.json()
-                                       if user_id == task.get('userId')]
+                                  if user_id == task.get('userId')]
         tasks_done_by_user_id = [task for task in total_tasks_by_user_id
-                                        if task.get('completed') == True]
+                                 if task.get('completed')]
 
         print("Employee {} is done with tasks({}/{}):".format(
             user.json().get('name'),
