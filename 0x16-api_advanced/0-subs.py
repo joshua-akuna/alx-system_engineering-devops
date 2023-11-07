09c3eb4070fb2775bc856382c660d0c11476abdf
Akuna Joshua <akunajoshua@gmail.com>
2023-11-07 15:34:26 +0100

    task 0.how many subs? : implemenatation files added

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''This module defines the number_of_subscribers function
+'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''The function queries the Reddit API and returns the number
+        of subscribers (not active users, total subscribers) for a
+        given subreddit
+        If an invalid subreddit is given, return 0
+    '''
+    agent = 'MyRedditBot/1.0'
+    # defines a custom User-Agent to avoid API rate-limiting issues
+    headers = {'User-Agent': agent}
+
+    # defines the URL for the Reddit API to get the information about
+    # the subreddit
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Sends a GET rewuest to the reddit API
+    res = requests.get(URL, headers)
+    if res.status_code == 200:
+        payload = res.json()
+        data = payload.get('data')
+        return data.get('subscribers')
+    return 0
