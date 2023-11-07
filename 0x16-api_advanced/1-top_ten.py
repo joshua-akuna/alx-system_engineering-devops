eed6270952b831b2b390dec58c939a75e3588a53
Akuna Joshua <akunajoshua@gmail.com>
2023-11-07 16:17:57 +0100

    task 1. top ten: implementation file added

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''This module defines the top_ten function
+'''
+
+import requests
+
+
+def top_ten(subreddit):
+    '''
+    '''
+    # defines the user-agent for the HTTP GET request
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    
+    # defines the URL for the http GET request
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    # defines the URL parameters for the request
+    params = {'limit': 10}
+
+    # sends the GET request
+    res = requests.get(URL, headers=headers, params=params)
+
+    if res.status_code == 200:
+        try:
+            data = res.json()
+            posts = data.get('data').get('children')
+            for post in posts:
+                post_title = post.get('data').get('title')
+                print(post_title)
+        except Exception:
+            return 0
+    return 0
